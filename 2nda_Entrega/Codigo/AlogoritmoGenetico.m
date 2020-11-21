1bc42b781f6b09355a8f8318fd896af3c5c8ada6
CarlosBravo00 <carlosemiliobravomarquez@gmail.com>
2020-11-21 15:51:17 -0600

    Fix mutuacion

--- 2nda_Entrega/Codigo/AlogoritmoGenetico.m
+++ 2nda_Entrega/Codigo/AlogoritmoGenetico.m
@@ -1,60 +1,69 @@
 clc;
 clear;
 close all;
- 
-%% Funcion Objetivo
-
-fun =@(x) sum(x.^4);
- 
 %% Parametros
- 
+inp = struct;
 % # Individuos en la poblacion
-tam = 2;
+inp.tam = 2;
 % Valores Minimos y Maximos
-vMin= -10;
-vMax= 10;
+inp.vMin= -100;
+inp.vMax= 100;
 % Numero de Iteraciones
-numIte = 20;
-% Distribucion para la mutuacion
-AB = -1:1:1;
-
+inp.numIte = 50;
+% Mutuacion
+inp.TazaMut = .8;
+inp.MinMut = -5;
+inp.MaxMut = 5;
 % Cantidad de dimenciones
-NumVari = 3; 
+inp.NumVari = 3; 
+% idBfm
+inp.idBfm = 28;
 
-%% LLenar la poblacion original 
+%% Funcion Objetivo
+bfm();
+fun =@(x) bfm(inp.idBfm, x);
  
-Poblacion = int16.empty(tam,0);
+%% Algoritmo Genetico
+
+Poblacion = zeros(inp.tam,0);
+MejorValor = zeros(inp.numIte,1);
  
-for i=1:tam
-    Poblacion = [Poblacion;randi([vMin vMax],1,NumVari)];  %#ok<*AGROW>
+for i=1:inp.tam
+    Poblacion = [Poblacion;randi([inp.vMin inp.vMax],1,inp.NumVari)];  %#ok<*AGROW>
 end
 
-%% Algoritmo Genetico
-MejorValor=zeros(numIte,1);
- 
-for Iteracion=1:numIte
+for Iteracion=1:inp.numIte
     %CrossOver
-    for i=1:tam
-        if(NumVari == 2)
-            if(i == tam)
-             Poblacion = [Poblacion; [Poblacion(i,1)  Poblacion(1,2)]];
+    Hijos = zeros(inp.tam,0);
+    for i=1:inp.tam
+        if(inp.NumVari == 2)
+            if(i == inp.tam)
+             Hijos = [Hijos; [Poblacion(i,1)  Poblacion(1,2)]];
             else
-             Poblacion = [Poblacion; [Poblacion(i,1)  Poblacion(i+1,2)]];
+             Hijos = [Hijos; [Poblacion(i,1)  Poblacion(i+1,2)]];
             end
         else
-            if(i == tam)
-             Poblacion = [Poblacion; [Poblacion(i,1)  Poblacion(1,2) Poblacion(1,3)]];
+            if(i == inp.tam)
+             Hijos = [Hijos; [Poblacion(i,1)  Poblacion(1,2) Poblacion(1,3)]];
             else
-             Poblacion = [Poblacion; [Poblacion(i,1)  Poblacion(i+1,2) Poblacion(i+1,3)]];
+             Hijos = [Hijos; [Poblacion(i,1)  Poblacion(i+1,2) Poblacion(i+1,3)]];
             end   
         end
     end 
-    %Poblacion es size 2*tam 
+    %Mutuacion
+    for i=1:inp.tam
+        for j=1:inp.NumVari
+          if( rand(1,1) < inp.TazaMut )
+             Hijos(i,j) = Hijos(i,j) + randi([inp.MinMut inp.MaxMut],1,1);
+          end
+       end
+    end
+    Poblacion = [Poblacion ; Hijos]; %Poblacion es size 2*tam 
     %Seleccion
     %Orden para seleccion competitiva 
-     NuevaPoblacion = int16.empty(tam,0);
-     randomOrder = randperm(tam*2);
-     for i=1:2:tam*2
+     NuevaPoblacion = zeros(inp.tam,0);
+     randomOrder = randperm(inp.tam*2);
+     for i=1:2:inp.tam*2
         %competencia de randomOrder[i] contr randomOrder[I+1]
         %Fitness
         if(fun(Poblacion(randomOrder(i),:)) < fun(Poblacion(randomOrder(i+1),:)))
@@ -64,24 +73,15 @@ for Iteracion=1:numIte
         end     
      end 
      Poblacion = NuevaPoblacion;
-     %Mutuacion
-     for i=1:tam
-        Poblacion(i,1) = Poblacion(i,1) + AB(randi(length(AB)));
-        Poblacion(i,2) = Poblacion(i,2) + AB(randi(length(AB)));
-        if(NumVari == 3)
-            Poblacion(i,3) = Poblacion(i,3) + AB(randi(length(AB)));
-        end 
-     end
      min = intmax('int16');
-      for i=1:tam
+      for i=1:inp.tam
         if(fun(Poblacion(i,:)) < min)
             min = fun(Poblacion(i,:));
             minVal = Poblacion(i,:);
         end
       end   
      disp(['Iteracion ' num2str(Iteracion) ': ' num2str(min)]);
-     MejorValor(Iteracion)=min;
-     
+     MejorValor(Iteracion)= min;
 end
  
 %% Results
