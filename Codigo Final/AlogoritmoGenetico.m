619232064b31cd76babbbe4053949a346e0287d4
CarlosBravo00 <carlosemiliobravomarquez@gmail.com>
2020-11-24 10:52:26 -0600

    Error: Int en mutuacion
    Corregido que solo se sumen valores enteros en la mutuacion y se creen unicamente soluciones enteras

--- Codigo Final/AlogoritmoGenetico.m
+++ Codigo Final/AlogoritmoGenetico.m
@@ -3,79 +3,70 @@ function [ MejorValor, PeorValor, PromedioValor, PobFinal, Solucion] = Alogoritm
 
 	fun =@(x) bfm(inp.idBfm, x);
     
-    Solucion = [];
-    PobFinal = zeros(inp.tam,0);
-    Poblacion = zeros(inp.tam,0);
+    Solucion = zeros(1,inp.NumVari+1);
+    PobFinal = [];
     MejorValor = zeros(inp.numIte,1);
     PeorValor = zeros(inp.numIte,1);
     PromedioValor = zeros(inp.numIte,1);
-
+    
+    Poblacion = zeros(inp.tam,inp.NumVari);
+    
     for i=1:inp.tam
-        Poblacion = [Poblacion;randi([inp.vMin inp.vMax],1,inp.NumVari)];  %#ok<*AGROW>
+        for j=1:inp.NumVari
+            Poblacion(i,j) = (inp.vMax-inp.vMin).*rand(1,1) + inp.vMin;
+        end
     end
     
     for Iteracion=1:inp.numIte
         %CrossOver
-        Hijos = zeros(inp.tam,0);
-        for i=1:inp.tam
-            if(inp.NumVari == 2)
-                if(i == inp.tam)
-                 Hijos = [Hijos; [Poblacion(i,1)  Poblacion(1,2)]];
-                else
-                 Hijos = [Hijos; [Poblacion(i,1)  Poblacion(i+1,2)]];
-                end
-            else
-                if(i == inp.tam)
-                 Hijos = [Hijos; [Poblacion(i,1)  Poblacion(1,2) Poblacion(1,3)]];
-                else
-                 Hijos = [Hijos; [Poblacion(i,1)  Poblacion(i+1,2) Poblacion(i+1,3)]];
-                end   
-            end
+        Hijos = zeros(inp.tam,inp.NumVari);
+        for i=1:inp.tam-1
+            Hijos(i,:) = [Poblacion(i,1)  Poblacion(i+1,2:end)];
         end 
+        Hijos(inp.tam,:) = [Poblacion(inp.tam,1)  Poblacion(1,2:end)];
         %Mutuacion de Hijos
         for i=1:inp.tam
             for j=1:inp.NumVari
               if( rand(1,1) <= inp.TasaMut )
-                 Hijos(i,j) = Hijos(i,j) + randi([inp.MinMut inp.MaxMut],1,1);
+                 Hijos(i,j) = Hijos(i,j) + (inp.MaxMut-inp.MinMut).*rand(1,1) + inp.MinMut;
               end
            end
         end
-        Poblacion = [Poblacion ; Hijos]; %Poblacion es size 2*tam 
+        allPoblacion = [Poblacion ; Hijos]; %Poblacion es size 2*tam 
         %Seleccion
         %Orden para seleccion competitiva 
-         NuevaPoblacion = zeros(inp.tam,0);
-         randomOrder = randperm(inp.tam*2);
-         for i=1:2:inp.tam*2
-            %competencia de randomOrder[i] contr randomOrder[I+1]
-            %Fitness
-            if(fun(Poblacion(randomOrder(i),:)) < fun(Poblacion(randomOrder(i+1),:)))
-                NuevaPoblacion = [NuevaPoblacion ; Poblacion(randomOrder(i),:)];
-            else 
-                NuevaPoblacion = [NuevaPoblacion ; Poblacion(randomOrder(i+1),:)];
-            end     
-         end 
-         Poblacion = NuevaPoblacion;
-         min = inf;
-         max = -inf;
-         Agen = [];
-         minAge = [];
-          for i=1:inp.tam
-            Agen = [ Agen ; [Poblacion(i,:) fun(Poblacion(i,:))]];  
-            if(fun(Poblacion(i,:)) < min)
-                min = fun(Poblacion(i,:));
-                minAge = [Poblacion(i,:) fun(Poblacion(i,:))];
-            end
-            if(fun(Poblacion(i,:)) > max)
-                max = fun(Poblacion(i,:));
-            end
-          end   
-         PobFinal = [PobFinal ; Agen];
-         MejorValor(Iteracion)= min;
-         PeorValor(Iteracion)= max;
-         PromedioValor(Iteracion)= (min + max)/2;
-         Solucion = minAge;
+        randomOrder = randperm(inp.tam*2);
+        cont = 1;
+        for i=1:2:inp.tam*2
+          %competencia de randomOrder[i] contr randomOrder[I+1]
+          %Fitness
+          if(fun(allPoblacion(randomOrder(i),:)) < fun(allPoblacion(randomOrder(i+1),:)))
+             Poblacion(cont,:) = allPoblacion(randomOrder(i),:);
+          else 
+             Poblacion(cont,:) = allPoblacion(randomOrder(i+1),:);
+          end 
+          cont = cont + 1;
+        end 
+        min = inf;
+        max = -inf;
+        Agen = zeros(inp.tam,inp.NumVari+1);
+        minAge = zeros(1,inp.NumVari+1);
+        for i=1:inp.tam
+          Agen(i,:) = [Poblacion(i,:) fun(Poblacion(i,:))];  
+          if(fun(Poblacion(i,:)) < min)
+              min = fun(Poblacion(i,:));
+              minAge = [Poblacion(i,:) fun(Poblacion(i,:))];
+          end
+          if(fun(Poblacion(i,:)) > max)
+              max = fun(Poblacion(i,:));
+          end
+        end   
+        PobFinal = [PobFinal ; Agen]; %#ok<AGROW>
+        MejorValor(Iteracion)= min;
+        PeorValor(Iteracion)= max;
+        PromedioValor(Iteracion)= (min + max)/2;
+        Solucion = minAge;
     end
-    Solucion
 end
 
  
